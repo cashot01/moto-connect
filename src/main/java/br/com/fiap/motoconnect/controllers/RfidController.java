0e20a70d877fa9c77a995fd2d705f4f1283a4b7d
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-08-24 18:22:05 -0300

    feat: implement I18 RfidController, rfid index e form

--- src/main/java/br/com/fiap/motoconnect/controllers/RfidController.java
+++ src/main/java/br/com/fiap/motoconnect/controllers/RfidController.java
@@ -2,8 +2,14 @@ package br.com.fiap.motoconnect.controllers;
 
 import br.com.fiap.motoconnect.models.Rfid;
 import br.com.fiap.motoconnect.services.RfidService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +17,12 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/rfid")
+@RequiredArgsConstructor
 public class RfidController {
     
     private final RfidService rfidService;
 
-    public RfidController(RfidService rfidService) {
-        this.rfidService = rfidService;
-    }
+    private final MessageSource messageSource;
 
     @GetMapping
     public String index(Model model) {
@@ -26,15 +31,28 @@ public class RfidController {
         return "rfid/index";
     }
 
+    //@GetMapping("/form")
+    //public String form() {
+    //    return "rfid/form";
+    //}
+
     @GetMapping("/form")
-    public String form() {
-        return "rfid/form";
+    public String form(Model model) {
+        model.addAttribute("rfid", new Rfid()); // Adiciona um novo objeto Rfid ao modelo
+        return "rfid/form"; // Retorna o template correto
     }
 
     @PostMapping("/form")
-    public String create(Rfid rfid, RedirectAttributes redirect) {
+    public String create(@Valid Rfid rfid, BindingResult result, RedirectAttributes redirect ) {
+
+        if (result.hasErrors()) {
+            return "rfid/form"; // Retorna o template correto em caso de erro
+        }
+
+        var message = messageSource.getMessage("rfid.create.success", null, LocaleContextHolder.getLocale());
         rfidService.save(rfid);
-        redirect.addFlashAttribute("message", "Rfid cadastrado com sucesso");
+        //redirect.addFlashAttribute("message", "Rfid cadastrado com sucesso");
+        redirect.addFlashAttribute("message", message);
         return "redirect:/rfid";
     }
 }

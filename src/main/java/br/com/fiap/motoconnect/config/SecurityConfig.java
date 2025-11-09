150e43f5ad4fb976e1da25a3e0cf1f38ffd8b546
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-11-09 15:46:54 -0300

    feat: update logout

--- src/main/java/br/com/fiap/motoconnect/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/motoconnect/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login")
+                        .logoutSuccessUrl("/login?logout=true")
                         .permitAll()
                 )
                 .exceptionHandling(exception -> exception

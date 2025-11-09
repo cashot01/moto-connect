bf7f26bcc825b644624673e03742deac61ecbe66
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-11-09 15:19:03 -0300

    feat: update SecurityConfig

--- src/main/java/br/com/fiap/motoconnect/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/motoconnect/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/login", "/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/", "/login", "/css/**", "/js/**", "/images/**").permitAll()
                         .requestMatchers("/perfil").authenticated()
                         .requestMatchers("/usuarios").hasRole("ADMIN")
                         .anyRequest().authenticated()
